3092e100a3b96bfcadba232aaf4e9135f995030e
Bogdan Kalitenko <kalitenkobv@gmail.com>
2018-04-07 12:21:34 +0300

    1) убрал цикл при подсчете стоимости; 2) исправил функцию выезда - сделал освобождение мест и удаление машины из HashMap

--- src/main/java/ru/sbt/jschool/session3/problem2/ParkingLot.java
+++ src/main/java/ru/sbt/jschool/session3/problem2/ParkingLot.java
@@ -52,6 +52,10 @@ public class ParkingLot implements ParkingService {
 
         // для автомобиля, который был на парковке
         Car car = carsOnParkingLot.get(carID);
+        // место освободилось
+        numberOfParkingSpaces++;
+        // удаляем с парковки
+        carsOnParkingLot.remove(carID, car);
 
         // если время выезда, меньше чем время въезда
         // ошибка? пока плату не берем
@@ -71,19 +75,42 @@ public class ParkingLot implements ParkingService {
         // общее время парковки
         long timeOnParking = finalTime - startTime;
 
+        long tmp = startTime % 24;
+        startTime = (tmp == 0) ? 24 : tmp;
+
+        tmp = finalTime % 24;
+        finalTime = (tmp == 0) ? 24 : tmp;
+
         long nightFactor = 2;
         long startNight = 23;
         long startDay = 6;
+        long lengthOfNight = 24 - startNight + startDay;
 
-        while(timeOnParking > 0) {
-            if ((startTime + timeOnParking) % 24 >= startNight || (startTime + timeOnParking) % 24 <= startDay)
-                cost += costPerHour * nightFactor;
-            else
-                cost += costPerHour;
-            timeOnParking--;
+        double costPerDay = (lengthOfNight * nightFactor + 24 - lengthOfNight) * costPerHour;
+        cost = timeOnParking / 24 * costPerDay;
+        timeOnParking %= 24;
+
+
+        // если осталось время
+        if(timeOnParking > 0) {
+
+            if (startTime >= startNight || startTime < startDay)
+                if (finalTime%24 <= startDay)
+                    cost += timeOnParking * nightFactor * costPerHour;
+                else{
+                    tmp = startTime >= startNight ? (startTime - startNight) : 0;
+                    cost += ((tmp + startDay) * nightFactor
+                            + timeOnParking - (tmp + startDay + 1)) * costPerHour;
+                }
+            else {
+                if (finalTime < startNight)
+                    cost += timeOnParking * costPerHour;
+                else
+                    cost += ((finalTime - startNight) * nightFactor
+                            + (timeOnParking - (finalTime - startNight))) * costPerHour;
+            }
         }
 
         return cost;
     }
-
 }
--- src/main/java/ru/sbt/jschool/session3/problem2/SimpleParkingLotTest.java
+++ src/main/java/ru/sbt/jschool/session3/problem2/SimpleParkingLotTest.java
@@ -6,7 +6,7 @@ package ru.sbt.jschool.session3.problem2;
 public class SimpleParkingLotTest {
 
     public static void main(String[] args) {
-        ParkingLot parking1 = new ParkingLot(3, 4.5);
+        ParkingLot parking1 = new ParkingLot(3, 1);
 
         System.out.println("Результат въезда:");
         System.out.println(parking1.driveIn(1, 0));
@@ -21,6 +21,18 @@ public class SimpleParkingLotTest {
         System.out.println(parking1.leave(2, 38) + "$");
         System.out.println(parking1.leave(3, 155) + "$");
         System.out.println(parking1.leave(4, 142) + "$");
+
+        System.out.println("Парковка полностью освободилась, пусть снова заполнится.");
+        System.out.println("Результат въезда:");
+        System.out.println(parking1.driveIn(1, 25));
+        System.out.println(parking1.driveIn(2, 6));
+        System.out.println(parking1.driveIn(3, 12));
+
+        System.out.println("Стоимость парковки:");
+        System.out.println(parking1.leave(1, 50) + "$");
+        System.out.println(parking1.leave(2, 30) + "$");
+        System.out.println(parking1.leave(3, 48) + "$");
+
 
 
 
